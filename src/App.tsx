dafd98fc862ada2381207a1185243a4e6d7b872b
Ben Smerd <bsmerd@adam.com.au>
2022-12-18 20:45:50 +1030

    init commit vite

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+//* This is a NFT viewer app marketplace
+
+//* WIll combine in the connect wallet for web3--- webjs library
+
+//* Users will be able to view their NFTs and interact...
+
+//* Maybe have a login database- MongoDB maybe
+
+//* Maybe allow users to interact and buy and sell- could be advanced and show can make a transaction happening
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      </div
+  )
+}
+
+export default App
