97def600d83c96e4227116882d4259308bb54862
Ben Smerd <bsmerd@adam.com.au>
2022-12-29 21:32:52 +1030

    update styling header

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -5,28 +5,24 @@ const Header = () => {
   const [navOn, setNavOn] = useState<string>("Home");
 
   return (
-    <div className="flex justify-between items-center p-3 border-b">
-      <h1 className="font-bold text-3xl">NFTView</h1>
-      <ul className="flex gap-4 items-center">
+    <div className="flex justify-between items-center p-2 border-b">
+      <h1 className="font-bold">NFTView</h1>
+      <ul className="flex gap-3 items-center">
         <Link
           onClick={(): void => setNavOn("Home")}
           to="/"
-          className={`text-lg cursor-pointer ${
-            navOn === "Home" && "font-bold"
-          }`}
+          className={`cursor-pointer ${navOn === "Home" && "font-bold"}`}
         >
           Home
         </Link>
         <Link
           onClick={(): void => setNavOn("Profile")}
           to="/profile"
-          className={`text-lg cursor-pointer ${
-            navOn === "Profile" && "font-bold"
-          }`}
+          className={`cursor-pointer ${navOn === "Profile" && "font-bold"}`}
         >
           Profile
         </Link>
-        <li className="text-lg border-solid border-2 border rounded-lg p-2 bg-gray-100 hover:bg-gray-300 cursor-pointer">
+        <li className="border-solid border-2 border rounded-lg p-2 bg-gray-100 hover:bg-gray-300 cursor-pointer">
           Connect Wallet
         </li>
       </ul>
